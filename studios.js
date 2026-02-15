033e8f319f4997789ab2d986e1c4a794814cc5a7
bodya-elven <bodyanumber11@gmail.com>
2026-02-15 19:59:16 +0200

    Update studios.js

--- studios.js
+++ studios.js
@@ -4,11 +4,10 @@
     function TMDBStudios() {
         var _this = this;
 
-        // 1. Локалізація
         if (Lampa.Lang) {
             Lampa.Lang.add({
                 tmdb_studios: {
-                    en: 'Studios',
+                    en: 'Production',
                     uk: 'Виробництво'
                 }
             });
@@ -21,19 +20,19 @@
                 if (e.type == 'complite') {
                     var card = e.data.movie;
                     if (card && (card.source == 'tmdb' || e.data.source == 'tmdb') && card.id) {
-                        var render = e.object.activity.render();
-                        _this.getStudios(render, card);
+                        _this.getStudios(e.object.activity.render(), card);
                     }
                 }
             });
 
             var style = document.createElement('style');
             style.innerHTML = `
-                .studios-icon-svg { 
+                .studios-icon-img { 
                     width: 1.4em; 
                     height: 1.4em; 
+                    object-fit: contain;
                     display: block;
-                    fill: currentColor; /* Іконка буде білою, як текст */
+                    filter: invert(1); 
                 }
                 .button--studios { 
                     display: flex; 
@@ -71,30 +70,24 @@
             if (!container.length || container.find('.button--studios').length) return;
 
             var title = Lampa.Lang.translate('tmdb_studios');
-            
-            // Вшита іконка кіноплівки (SVG)
-            var icon = '<svg class="studios-icon-svg" viewBox="0 0 512 512"><path d="M414.1 113c-4.4-1.6-9.1-.8-12.7 2.1l-50.6 40.5V112c0-26.5-21.5-48-48-48H48C21.5 64 0 85.5 0 112v288c0 26.5 21.5 48 48 48h254.9c26.5 0 48-21.5 48-48v-43.6l50.6 40.5c3.6 2.9 8.3 3.7 12.7 2.1 4.4-1.6 7.3-5.8 7.3-10.4V123.4c0-4.6-2.9-8.8-7.3-10.4zM302.9 400H48V112h254.9v288zm161.1-23.4l-64-51.2V186.6l64-51.2v241.2z"/></svg>';
+            var icon = '<img src="https://bodya-elven.github.io/Different/film.svg" class="studios-icon-img" />';
             
             var button = $('<div class="full-start__button selector view--category button--studios">' + icon + '<span>' + title + '</span></div>');
 
             button.on('hover:enter click', function () {
                 var items = studios.map(function(s) {
-                    return { 
-                        title: s.name, 
-                        id: s.id 
-                    };
+                    return { title: s.name, id: s.id };
                 });
 
                 Lampa.Select.show({
                     title: title, 
                     items: items,
                     onSelect: function (selectedItem) {
-                        // Відкриваємо сторінку студії (компанії). 
-                        // Lampa автоматично підтягне розділення на фільми/серіали
+                        // Змінено компонент на 'category' для правильного відображення як на фото
                         Lampa.Activity.push({ 
                             url: 'company/' + selectedItem.id, 
                             title: selectedItem.title, 
-                            component: 'category_full', 
+                            component: 'category', 
                             source: 'tmdb', 
                             page: 1 
                         });
@@ -110,8 +103,8 @@
         };
     }
 
-    if (!window.plugin_tmdb_studios_v2) {
-        window.plugin_tmdb_studios_v2 = new TMDBStudios();
-        window.plugin_tmdb_studios_v2.init();
+    if (!window.plugin_tmdb_studios_v3) {
+        window.plugin_tmdb_studios_v3 = new TMDBStudios();
+        window.plugin_tmdb_studios_v3.init();
     }
 })();
