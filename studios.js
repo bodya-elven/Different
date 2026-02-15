6420a042e7ca7ef45351351f690c6dcdc9a8e1f9
bodya-elven <bodyanumber11@gmail.com>
2026-02-15 19:46:11 +0200

    Update studios.js

--- studios.js
+++ studios.js
@@ -9,15 +9,7 @@
             Lampa.Lang.add({
                 tmdb_studios: {
                     en: 'Studios',
-                    uk: 'Студії'
-                },
-                tmdb_studios_movies: {
-                    en: 'Movies',
-                    uk: 'Фільми'
-                },
-                tmdb_studios_tv: {
-                    en: 'TV Series',
-                    uk: 'Серіали'
+                    uk: 'Виробництво'
                 }
             });
         }
@@ -38,20 +30,16 @@
             var style = document.createElement('style');
             style.innerHTML = `
                 .studios-icon-svg { 
-                    width: 1.6em; 
-                    height: 1.6em; 
+                    width: 1.4em; 
+                    height: 1.4em; 
                     display: block;
-                    fill: none;
-                    stroke: currentColor;
-                    stroke-width: 2;
-                    stroke-linecap: round;
-                    stroke-linejoin: round;
+                    fill: currentColor; /* Іконка буде білою, як текст */
                 }
                 .button--studios { 
                     display: flex; 
                     align-items: center; 
                     justify-content: center; 
-                    gap: 0.4em; 
+                    gap: 0.5em; 
                 }
             `;
             document.head.appendChild(style);
@@ -59,7 +47,7 @@
 
         this.getStudios = function (html, card) {
             var method = (card.original_name || card.name) ? 'tv' : 'movie';
-            var url = Lampa.TMDB.api(method + '/' + card.id + '?api_key=' + Lampa.TMDB.key());
+            var url = Lampa.TMDB.api(method + '/' + card.id + '?api_key=' + Lampa.TMDB.key() + '&language=' + Lampa.Storage.get('language', 'uk'));
 
             $.ajax({
                 url: url,
@@ -83,21 +71,33 @@
             if (!container.length || container.find('.button--studios').length) return;
 
             var title = Lampa.Lang.translate('tmdb_studios');
-            // Іконка "Будівля/Студія"
-            var icon = '<svg class="studios-icon-svg" viewBox="0 0 24 24"><path d="M3 21h18M3 7v14M21 7v14M9 21V11h6v10M7 3h10l4 4H3l4-4z"></path></svg>';
+            
+            // Вшита іконка кіноплівки (SVG)
+            var icon = '<svg class="studios-icon-svg" viewBox="0 0 512 512"><path d="M414.1 113c-4.4-1.6-9.1-.8-12.7 2.1l-50.6 40.5V112c0-26.5-21.5-48-48-48H48C21.5 64 0 85.5 0 112v288c0 26.5 21.5 48 48 48h254.9c26.5 0 48-21.5 48-48v-43.6l50.6 40.5c3.6 2.9 8.3 3.7 12.7 2.1 4.4-1.6 7.3-5.8 7.3-10.4V123.4c0-4.6-2.9-8.8-7.3-10.4zM302.9 400H48V112h254.9v288zm161.1-23.4l-64-51.2V186.6l64-51.2v241.2z"/></svg>';
             
             var button = $('<div class="full-start__button selector view--category button--studios">' + icon + '<span>' + title + '</span></div>');
 
             button.on('hover:enter click', function () {
                 var items = studios.map(function(s) {
-                    return { title: s.name, studio_data: s };
+                    return { 
+                        title: s.name, 
+                        id: s.id 
+                    };
                 });
 
                 Lampa.Select.show({
                     title: title, 
                     items: items,
                     onSelect: function (selectedItem) {
-                        _this.showTypeMenu(selectedItem.studio_data);
+                        // Відкриваємо сторінку студії (компанії). 
+                        // Lampa автоматично підтягне розділення на фільми/серіали
+                        Lampa.Activity.push({ 
+                            url: 'company/' + selectedItem.id, 
+                            title: selectedItem.title, 
+                            component: 'category_full', 
+                            source: 'tmdb', 
+                            page: 1 
+                        });
                     },
                     onBack: function() {
                         Lampa.Controller.toggle('full_start');
@@ -108,34 +108,10 @@
             container.append(button);
             Lampa.Controller.toggle('full_start');
         };
-
-        this.showTypeMenu = function(studio) {
-            var menu = [
-                { title: Lampa.Lang.translate('tmdb_studios_movies'), method: 'movie' },
-                { title: Lampa.Lang.translate('tmdb_studios_tv'), method: 'tv' }
-            ];
-
-            Lampa.Select.show({
-                title: studio.name, 
-                items: menu,
-                onSelect: function(item) {
-                    Lampa.Activity.push({ 
-                        url: 'discover/' + item.method + '?with_companies=' + studio.id + '&sort_by=popularity.desc', 
-                        title: studio.name + ' - ' + item.title, 
-                        component: 'category_full', 
-                        source: 'tmdb', 
-                        page: 1 
-                    });
-                },
-                onBack: function() {
-                    Lampa.Controller.toggle('full_start');
-                }
-            });
-        };
     }
 
-    if (!window.plugin_tmdb_studios) {
-        window.plugin_tmdb_studios = new TMDBStudios();
-        window.plugin_tmdb_studios.init();
+    if (!window.plugin_tmdb_studios_v2) {
+        window.plugin_tmdb_studios_v2 = new TMDBStudios();
+        window.plugin_tmdb_studios_v2.init();
     }
 })();
