6adf85beffcff77185223e65b7ac4e26bc268bc2
bodya-elven <bodyanumber11@gmail.com>
2026-04-14 11:59:13 +0300

    Update mdb_ratings.js

--- mdb_ratings.js
+++ mdb_ratings.js
@@ -1295,7 +1295,7 @@ function applyDynamicColorToIcon($iconElement, colorData, isWide) {
 
     // Тінь: чорна (0.5) для світлих кольорів (>76), біла (0.5) для темних
     var shadowColor = brightness > 76 ? 'rgba(0, 0, 0, 0.1)' : 'rgba(255, 255, 255, 0.1)';
-    var dropShadowStyle = 'drop-shadow(0px 0px 1px ' + shadowColor + ')';
+    var dropShadowStyle = 'drop-shadow(0px 1px 0px ' + shadowColor + ')';
 
     if (isWide) {
         // --- ШИРОКІ ЛОГОТИПИ ---
